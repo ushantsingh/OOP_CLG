8f747831d4e51a2897807dededd5b6d2d8681dd0
Ushant Singh <ushantsingh4@gmail.com>
2025-12-01 12:10:58 +0530

    lect 6

--- lec6/Exception_handling.cpp
+++ lec6/Exception_handling.cpp
@@ -3,4 +3,43 @@
 C++ implements exception handling using three keywords. 
 --> try :- Try is a scoped block which should enclose the line of code which may generate exception 
 --> Catch :- Catch is also a scoped block that excepts an argument this part is used to display meaningful messages once an error is called 
---> throw*/
+--> throw
+
+
+
+Q. When we can handle errors using if else statement . why should we use try catch */
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    try
+    {
+        int x = 0;
+        if(x==1)
+            throw -1;
+            else if(x==2)
+                throw 'a';
+                else if(x==3)
+                    throw 2.5;
+    }
+    catch(int &e)
+    {
+        cout<<"int error"<<endl;
+    }
+    catch(char &e)
+    {
+        cout<<"char error"<<endl;
+    }
+    catch(double &e)
+    {
+        cout<<"Double error"<<endl;
+    }
+    catch(float &e)
+    {
+        cout<<"unknown  error"<<endl;
+    }
+    
+}
